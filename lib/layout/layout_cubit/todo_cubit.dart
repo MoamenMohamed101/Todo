36fd17d5558299ca20d50f3f41c2366cca64526c
Moamen <mmoamenmohamed101@gmail.com>
2024-10-14 21:35:57 +0300

    insert and get with cubit

--- lib/layout/layout_cubit/todo_cubit.dart
+++ lib/layout/layout_cubit/todo_cubit.dart
@@ -1,36 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:todo/layout/layout_cubit/todo_states.dart';
 import 'package:todo/modules/archive_screen.dart';
 import 'package:todo/modules/done_screen.dart';
 import 'package:todo/modules/tasks_screen.dart';
 
-class TodoLayoutCubit extends Cubit<TodoLayoutStates>{
-  TodoLayoutCubit() : super (TodoLayoutInitialState());
+class TodoLayoutCubit extends Cubit<TodoLayoutStates> {
+  TodoLayoutCubit() : super(TodoLayoutInitialState());
 
-  static TodoLayoutCubit get(context)=> BlocProvider.of(context);
+  static TodoLayoutCubit get(context) => BlocProvider.of(context);
 
   List<Map> tasks = [];
   int currentIndex = 0;
   bool isBottomSheetShow = false;
   IconData isIcon = Icons.edit;
+  Database? dataBase;
 
   late List<Widget> screens = [
-    TasksScreen(tasks),
+    TasksScreen(),
     const DoneScreen(),
     const ArchiveScreen(),
   ];
   List<String> titles = ["Tasks Screen", "Done Screen", "Archive Screen"];
 
-  void changeIndex(int index)
-  {
+  void changeIndex(int index) {
     currentIndex = index;
     emit(TodoLayoutChangeIndexState());
   }
 
-  void changeIcons()
-  {
+  void changeIcons() {
     isBottomSheetShow ? isIcon = Icons.add : isIcon = Icons.edit;
     emit(TodoLayoutChangeIconState());
   }
+
+  // void changeIcons(bool isShow,IconData icon)
+  // {
+  //   isBottomSheetShow = isShow;
+  //   isIcon = icon;
+  // emit(TodoLayoutChangeIconState());
+  // }
+
+  void createDataBase() {
+    openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (dataBase, version) {
+        debugPrint("database created");
+        dataBase
+            .execute(
+                'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
+            .then((value) {
+          debugPrint("tables created");
+        }).catchError((error) {
+          debugPrint("error when creating tables: ${error.toString()}");
+        });
+      },
+      onOpen: (dataBase) {
+        getDataBase(dataBase).then((onValue) {
+          tasks = onValue;
+          emit(TodoLayoutGetDataBaseSuccessState());
+        });
+        debugPrint("database opened");
+      },
+    ).then((onValue) {
+      dataBase = onValue;
+      emit(TodoLayoutCreateDataBaseState());
+    });
+  }
+
+  Future insertDataBase({
+    required String title,
+    required String date,
+    required String time,
+  }) async {
+    return await dataBase!.transaction((action) => action
+            .rawInsert(
+                'INSERT INTO tasks (title,date,time,status) VALUES("$title","$date","$time","new")')
+            .then((value) {
+          debugPrint("$value inserted successfully");
+          getDataBase(dataBase).then((onValue){
+            tasks = onValue;
+            emit(TodoLayoutGetDataBaseSuccessState());
+          });
+          emit(TodoLayoutInsertDataBaseSuccessState());
+        }).catchError((error) {
+          debugPrint("error will inserting $error");
+          emit(TodoLayoutInsertDataBaseErrorState());
+        }));
+  }
+
+  Future<List<Map>> getDataBase(dataBase) async =>
+      await dataBase!.rawQuery('SELECT * FROM tasks');
 }
--- lib/modules/tasks_screen.dart
+++ lib/modules/tasks_screen.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/layout/layout_cubit/todo_cubit.dart';
+import 'package:todo/layout/layout_cubit/todo_states.dart';
 import 'package:todo/shared/components/components.dart';
 
 class TasksScreen extends StatelessWidget {
-  List<Map> tasks = [];
-
-  TasksScreen(this.tasks, {super.key});
+  TasksScreen( {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (BuildContext context, int index) =>
-          buildTaskItem(tasks[index]),
-      separatorBuilder: (BuildContext context, int index) => Padding(
-        padding: const EdgeInsetsDirectional.only(start: 15),
-        child: Container(
-          height: 1,
-          width: double.infinity,
-          color: Colors.grey,
-        ),
-      ),
-      itemCount: tasks.length,
+    return BlocConsumer<TodoLayoutCubit,TodoLayoutStates>(
+      builder: (BuildContext context, state) {
+        TodoLayoutCubit cubit = TodoLayoutCubit.get(context);
+        return ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return buildTaskItem(cubit.tasks[index]);
+          },
+          separatorBuilder: (BuildContext context, int index) => Padding(
+            padding: const EdgeInsetsDirectional.only(start: 15),
+            child: Container(
+              height: 1,
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+          ),
+          itemCount: cubit.tasks.length,
+        );
+      },
+      listener: (BuildContext context, Object? state) {  },
     );
   }
 }
